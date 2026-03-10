b6e854463ae7ca9e6fe61d420f7650c7baa2ca19
azfarsamin <azfar.samin@gmail.com>
2026-03-10 19:39:00 -0400

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -9,15 +9,19 @@ export default function sitemap() {
       lastModified: new Date(),
     },
     {
-      url: "https://cccframework.ca/work/aerc-2026",
-      lastModified: new Date(),
-    },
-    {
       url: "https://cccframework.ca/work/debt-money-interest",
       lastModified: new Date(),
     },
     {
       url: "https://cccframework.ca/work/reason-revelation-architecture-truth",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://cccframework.ca/work/aerc-2026",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://cccframework.ca/articles/outsourced-thinking-or-misplaced-anxiety",
       lastModified: new Date(),
     },
   ];
