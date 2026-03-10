adee5f67c36b2ef116a254e9cec9252cdfa55976
azfarsamin <azfar.samin@gmail.com>
2026-03-10 18:04:14 -0400

    Create sitemap.ts

---
+++ app/sitemap.ts
@@ -0,0 +1,24 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://cccframework.ca",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://cccframework.ca/work",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://cccframework.ca/work/aerc-2026",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://cccframework.ca/work/debt-money-interest",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://cccframework.ca/work/reason-revelation-architecture-truth",
+      lastModified: new Date(),
+    },
+  ];
+}
